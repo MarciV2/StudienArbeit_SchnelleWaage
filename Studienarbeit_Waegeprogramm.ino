c1484ac9262c38f9ad9077d29bc2b98726e70b20
Marcel V <37421507+MarciV2@users.noreply.github.com>
2022-07-11 11:06:32 +0200

    Update and rename Studienarbeit_SD_Switch_Led.ino to Studienarbeit_Waegeprogramm.ino

--- Studienarbeit_Waegeprogramm.ino
+++ Studienarbeit_Waegeprogramm.ino
@@ -93,7 +93,6 @@ if(digitalRead(switchPin)){
   if(myFile){
       unsigned long wert=hx711.read();
       //Kalibrierten Wert ausrechnen (nicht exakt!!)
-      // unsigned long wertCal=(wert-8297724)/1800;
       unsigned long wertCal=(wert-8381379)/1767;
       if(wertCal>2000000) wertCal=0;
       myFile.print(millis()-millisecs);
